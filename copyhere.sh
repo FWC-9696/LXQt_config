f816b06ff01ad57f34e215dcd20a95267ebbf8de
FWC-9696 <jrichter96@hotmail.com>
2022-12-14 20:02:08 -0500

    GNOME Dark Mode & Wallpapers

--- copyhere.sh
+++ copyhere.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cp -R /home/$USER/.config/autostart /home/$USER/.config/gtk-3.0 /home/$USER/.config/lxqt /home/$USER/.config/openbox /home/$USER/.config/pcmanfm-qt .
+cp -R /home/$USER/.config/autostart /home/$USER/.config/gtk-3.0 /home/$USER/.config/lxqt /home/$USER/.config/openbox /home/$USER/.config/pcmanfm-qt /home/$USER/.config/dconf /home/$USER/.config/wallpaper .
